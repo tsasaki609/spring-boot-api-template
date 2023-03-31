7922d8dff6c689dc1be80195110d6a8a42109789
Takashi Sasaki <tsasaki609@gmail.com>
2023-03-31 20:17:21 +0900

    recieve request parameter

--- src/main/java/com/example/demo/presentation/DemoController.java
+++ src/main/java/com/example/demo/presentation/DemoController.java
@@ -1,21 +1,42 @@
 package com.example.demo.presentation;
 
 import com.example.demo.domain.Demo;
+import com.example.demo.domain.DemoId;
+import java.util.List;
+import lombok.Data;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping(path = "/v1/demo", produces = "text/event-stream")
 public class DemoController {
-  public Mono<Demo> get(DemoRequest request) {
+  @GetMapping("/{demo-id}")
+  public Mono<Demo> get(@PathVariable("demo-id") DemoId id) {
     return Mono.empty();
   }
 
-  public Flux<Demo> list() {
+  @GetMapping
+  public Flux<Demo> list(ListDemoRequest request) {
     return Flux.empty();
   }
 
-  public static class DemoRequest {
-    private String demoId;
+  @Data
+  public static class ListDemoRequest {
+    private List<DemoId> demoId;
+  }
+
+  @Component
+  public static class DemoIdConverter implements Converter<String, DemoId> {
+    @Override
+    public DemoId convert(String source) {
+      return new DemoId(Integer.parseInt(source));
+    }
   }
 }
