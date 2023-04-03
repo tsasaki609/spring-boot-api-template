810e5d331a52c57382374aa5e1130c9b33ab5458
Takashi Sasaki <tsasaki609@gmail.com>
2023-04-03 09:00:07 +0900

    commenting

--- src/main/java/com/example/demo/usecase/DemoUseCase.java
+++ src/main/java/com/example/demo/usecase/DemoUseCase.java
@@ -11,6 +11,10 @@ import reactor.core.publisher.Mono;
 @Service
 @RequiredArgsConstructor
 public class DemoUseCase {
+
+  // TODO 独自に定義した型に対するCodecを登録する方法がないっぽい？
+  // https://github.com/r2dbc/r2dbc-h2/issues/178
+  // どうしても解決できなければリポジトリ側で変換する
   private final DemoRepository demoRepository;
 
   public Mono<Demo> findById(DemoId id) {
